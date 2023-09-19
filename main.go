3a697be038d5dedddac27fe80c0c856cc3f50273
Rldardon <Raul.hbk04@gmail.com>
2023-09-19 08:29:59 -0600

    v1.0

---
+++ main.go
@@ -0,0 +1,12 @@
+// iniciar en go:  go mod init
+// correr programa: go run main.go
+// crear .exe: go build main.go
+package main
+
+import (
+	"github.com/Rldardon/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
