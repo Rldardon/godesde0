ecfaea30adb636c0a2aba992cc2653d28724ae47
Rldardon <raul.hbk04@gmail.com>
2023-11-26 02:47:32 -0600

    Cambios

---
+++ ejercicios/ejercicio01.go
@@ -0,0 +1,5 @@
+package ejercicios
+
+func ConviertoaNumero(numero int) string {
+	valor := int
+}
--- main.go
+++ main.go
@@ -5,13 +5,27 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(58997)
+	/*estado, texto := variables.ConviertoaTexto(58997)
 
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es Windows", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
